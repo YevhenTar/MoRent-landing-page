91109a7311e75869bece006f5d1191ce5b011a80
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-08-21 14:45:20 +0300

    Add horizontal scroll to car catalog

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const recentCarBtn = $('#recent-car-btn');
+const recomendedCarBtn = $('#recomended-car-btn');
+
+const recentCarBlock = $('#recent-car-block');
+const recomendedCarBlock = $('#recomended-car-block');
+
+let recentCarCheck = false;
+let recomendedCarCheck = false;
+
+
+recentCarBtn.on('click', function (e) {
+    e.preventDefault();
+    if (recentCarCheck) {
+        recentCarBtn.removeClass('is-active');
+        recentCarBlock.removeClass('is-active');
+        recentCarCheck = false;
+    } else {
+        recentCarBtn.addClass('is-active');
+        recentCarBlock.addClass('is-active');
+        recentCarCheck = true;
+    }
+});
+
+recomendedCarBtn.on('click', function (e) {
+    e.preventDefault();
+    if (recomendedCarCheck) {
+        recomendedCarBtn.removeClass('is-active');
+        recomendedCarBlock.removeClass('is-active');
+        recomendedCarCheck = false;
+    } else {
+        recomendedCarBtn.addClass('is-active');
+        recomendedCarBlock.addClass('is-active');
+        recomendedCarCheck = true;
+    }
+});
